3db1e965ac696e2d3d80ff5aea8593e126237886
Ivan Kosolapov <ikosolapov@fastmail.com>
2022-04-06 23:11:43 +0400

    Unused variables were blocking execution flow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,6 @@ variable "region" {
   type = string
 }
 
-variable "aws_access_key_id" {
-  type = string
-}
-variable "aws_secret_key" {
-  type = string
-}
-
 # Server configuration
 variable "server_image" {
   type = string
